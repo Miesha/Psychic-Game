f72ecc7cc41e77808d0a0b6a26cdaa61337d2432
Kamiesha Davis <mskamiesha@gmail.com>
2019-06-25 12:46:47 -0500

    linked sheets and psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+
+var alpha = [];
+var wins = 0;
+var losses = 0;
+var correctGuess = true;
+var displayGuess = 0;
+
+
+        
+// char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+// psuedo code 
+/* create an onkey function?
+/store on key  info and display it?
+/create a random alphabet generator
+/create number to guess
+/create a loop for 9 guesses ? 
+/create if true statement
+/store number of wins 
+/store number of loss
+Good Luck you can do it !*/
+
