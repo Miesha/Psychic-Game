367291c1307b936dd7739e4862162f49c61160cd
Kamiesha Davis <mskamiesha@gmail.com>
2019-06-29 15:58:20 -0500

    final game is working

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-A README is often the first item a visitor will see when visiting your repository. README files typically include information on:
 
-What the project does
-Why the project is useful
-How users can get started with the project
-Where users can get help with your project
-Who maintains and contributes to the project
+This project is called The Psychic Game. 
+
+The Psychic Game challenges the user to guess a letter that matches the computer's choice. 
+Each time you guess correctly the computer will add a win to your total win count. 
+You are given 9 guesses. The computer will display each guess. After the 9th guess the computer will add a loss to your losses total. 
+Start by pressing any letter on the keyboard. 
+
+This project is meant as an introduction to java script. This project focusses on variables, arrarys, if/else statements and connecting java to html. 
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,124 +1,71 @@
 
-var computerChoices= ['a', 'b', 'c', 'd']
-// var computerChoices= ['b', 'c','d', 'e', 'f', 'g','h', 'i','j', 'k','l', 'm' ,'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var computerChoices= ['a', 'b', 'c','d', 'e', 'f', 'g','h', 'i','j', 'k','l', 'm' ,'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 console.log(computerChoices.length);
-     // I need to reduce computerChoices or game will go on forever.
-    // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-var numberOfChoices = ['9', '8', '7', '6', '5', '4', '3', '2', '1']
-console.log(numberOfChoices);    
+var computerGuess;
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log('computer guess :  ' + computerGuess)
+
+var numberOfChoices = 9;
+console.log(numberOfChoices);
 
 var wins = 0;
 var losses = 0;
-var userGuessed = [];
-// var userGuess =("");
-
-
-
-// var userChoiceText = document.getElementById("userchoice-text");
-// var computerChoiceText = document.getElementById("computerchoice-text");
-// var winsText = document.getElementById("wins-text");
-// var lossesText = document.getElementById("losses-text");
-
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerGuess)
-
-
-
-
-
+var userGuess
+var letter = []
   
 
-document.onkeyup = function(event) {
-  
-    // Determines which key was pressed.
-  
+function reset() {
+  // reset letter or userGuess
+  // 
+  letter = []
+  document.getElementById("userGuess").innerHTML = letter;
+  numberOfChoices = []
+  document.getElementById("numberOfChoices").innerHTML = numberOfChoices;
+  //
+}
+
+    document.onkeyup = function(event) {
+
     var userGuess = event.key;
     userGuess = userGuess.toLowerCase();
     console.log(userGuess);
-    var userGuessed = [""];
-    
-    
-    // console.log('number of choices left' + i);
-    // console.log(numberOfChoices[i]);
-    
-    // document.getElementById = function() {
-    //  (local var) userGuess =(multiGuess[i])
-    // };
-    
-   
-    // Only run the following code block if the user presses "r" or "p" or "s".
-    
-    // for (var i = 0; i < numberOfChoices.lenth; i++){
-    if (userGuess === computerGuess) {
-        //  userGuess = storeUserguess
-        // Alert the userGuess and computerGuess
-        alert(userGuess + " is correct. You are a psychic!");
-        // userGuessText.textContent = "You chose: " + userGuess;
-        wins++;
+    letter.push(userGuess);
 
-        // document.getElementById("wins-text");
-        console.log("wins:" + wins);
-        document.getElementById("wins").innerHTML = wins;
-        // break;
-      
-      
-      }
-    
-      else {
-        userGuessed.push(String.fromCharCode(userGuess));   
-        alert("that ain't it");
-        // userGuessed.push(String.fromCharCode(userGuess));
-        losses++;
-        console.log("losses:" + losses);
-        console.log(userGuessed);
-        document.getElementById("losses").innerHTML = losses;
+    document.getElementById("userGuess").innerHTML = letter;
 
-      } 
-    // }
       
-    }
+        if (userGuess === computerGuess ) {
+          alert(userGuess + "  is correct. You are psychic!");
+          wins++;
+          document.getElementById("wins").innerHTML = wins;
+          console.log("wins:" + wins);
+           reset ()
       
+           }
+          
+        
+          
+        
+        
+        else {
+          numberOfChoices = numberOfChoices - 1;
+          console.log("numberOfChoices : " + numberOfChoices )
+          alert("that ain't it");
+          console.log("losses:" + losses);
+          
+          document.getElementById("numberOfChoices").innerHTML = numberOfChoices;
+          if (numberOfChoices === 0) {
+            losses++;
+            document.getElementById("losses").innerHTML = losses;         
+            console.log("losses  : " + losses);
+            reset ()
+          }
+          
 
-    //loop hasn't been established 
-    // wins and losses are displaying on html , clears on refresh only. 
+          
+
+          }
+        }
       
     
-  //   function random_number(num) {  // New function called random_choice that takes one parameter, num (or a number)
-  //     // Get a random number between 0 and a passed-in number
-  //     var num = num || 4  // If no number passed in, default to 4
-  //     return Math.floor(Math.random() * num); // Round the answer down (floor) of a random number between 0 and 1 and multiply it by a number. Then return a value and exit the function.
-  // }
-
-
-
-
-// if(correctGuess.indexOf(aphla.toLowerCase()) > -1){
-//     alert(variableforcorrectguess + 'is exactly what I was thinking !');     
-// }
-
-// else  {
-//   document.write( userChoice + "ain't it " + numGuess + 'more to go!');
-// }
-
-
-// char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-// psuedo code 
-/* create an onkey function? DONE
-/store on key  info and display it?
-/create a random alphabet generator
-
-/create a loop for 9 guesses ? 
-/create if true statement
-/store number of wins 
-/store number of loss
-Good Luck you can do it !*/
-
-/*questions :
-is there a shorter way to write the array?*/
-// How to I get computer guess to hold 1 guess for the user's nine choices
-
-/// userGuess = ""  empty string
-/// then pass these 
-
-
